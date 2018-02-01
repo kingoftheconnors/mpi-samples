ba8e51a8506fed3a3a0ee12de6bf17829de218ed
kingoftheconnors <Connormcp777@gmail.com>
2018-02-01 10:10:56 -0500

    Worked on Dense_matrix

---
+++ Matrix/DenseMatrix.h
@@ -0,0 +1,17 @@
+/*
+ * DenseMatrix.h
+ *
+ *  Created on: Jan 25, 2018
+ *      Author: Connor
+ */
+
+#ifndef DENSEMATRIX_H_
+#define DENSEMATRIX_H_
+
+class Dense_Matrix {
+public:
+	Dense_Matrix();
+	virtual ~Dense_Matrix();
+};
+
+#endif /* DENSEMATRIX_H_ */
