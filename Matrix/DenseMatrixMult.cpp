ee6b8b3a7575b55ef7e07d46265eaa9cd7eebaea
kingoftheconnors <Connormcp777@gmail.com>
2018-02-14 12:23:02 -0500

    Made Multiply and MatrixTest function with Dense Matrix

--- Matrix/DenseMatrixMult.cpp
+++ Matrix/DenseMatrixMult.cpp
@@ -1,38 +1,27 @@
 
-//#include "DenseMatrixMult.h"
-//#include <iostream>
-//using namespace std;
-/*
-class DenseMatrixMult {
+#include "DenseMatrixMult.h"
 
-public:
-  
-static void Multiply(Dense_Matrix &C, const Dense_Matrix &A, const Dense_Matrix &B)  // C = A * B
+template <typename T>
+int Multiply(Dense_Matrix<T> &C, const Dense_Matrix<T> &A, const Dense_Matrix<T> &B)  // C = A * B
 {
-  
-  if(A.rows() == B.cols()) {
-    for(int i=0; i<A.getCols(); i++) {
-      for(int j=0; j<A.getRows(); j++) {
-        for(int k=0; k<B.getRows(); k++) {
-	  //A=Row-Oriented and B=Row-Oriented
-	  C(i, k) += A(i, j) * B(j, k);
-	} 
-      }  
-    }
+
+  //Check for compatibility
+  if( (A.cols() != B.rows()) || (C.rows() != A.rows()) || (C.cols() != B.cols()) ) {
+    return -1;
   }
+
+  for(int i=0; i<A.getCols(); i++) {
+    for(int j=0; j<A.getRows(); j++) {
+      T temp = (T) 0.0;
+        for(int k=0; k<B.getRows(); k++) {
+	//A=Row-Oriented and B=Row-Oriented
+	temp += A(i, j) * B(j, k);
+      }
+	
+      C(i,k) = temp;
+    }  
+  }
+
+    return 1;
 }
 
-};
-*/
-
-/*int main() {
-  Dense_Matrix AA(2,3);
-  Dense_Matrix BB(2,3);
-  Dense_Matrix CC(2,2);
-
-  DenseMatrixMult::Multiply(CC, BB, AA);
-  cout << "Working";
-  cout << CC(1,1);
-
-  return 0;
-}*/
--- Matrix/DenseMatrixMult.h
+++ Matrix/DenseMatrixMult.h
@@ -3,11 +3,12 @@
 
 #include "DenseMatrix.h"
 
-class DenseMatrixMult{
+//class DenseMatrixMult{
 
- public:
-  static void Multiply(Dense_Matrix &C, Dense_Matrix &A, Dense_Matrix &B) {  // C = A * B
-    for(int i=0; i<A.cols(); i++) {
+  template<typename T>
+  int Multiply(Dense_Matrix<T> &C, Dense_Matrix<T> &A, Dense_Matrix<T> &B);
+  /*{
+   for(int i=0; i<A.cols(); i++) {
       for(int j=0; j<A.rows(); j++) {
         for(int k=0; k<B.rows(); k++) {
 	  C(i, k) += A(i, j) * B(j, k);
@@ -15,6 +16,6 @@ class DenseMatrixMult{
       }
     }
   }
-};
+};*/
 
 #endif
